9646e9c7e08f3ef9e8d22e674149820629d8a0f2
thaninpong <thaninpong@perfenterprise.com>
2018-06-20 14:19:13 +0700

    n

--- Training.TestDrivenDevelopement/TDD.TestProject/UnitTest1.cs
+++ Training.TestDrivenDevelopement/TDD.TestProject/UnitTest1.cs
@@ -18,39 +18,39 @@ namespace TDD.TestProject
 
 
 
-        [Fact]
-        public void Test()
-        {
-            Program xxx = new Program();
-            var resual = xxx.Grad(80);
-            Assert.Equal("A", resual);
+        //[Fact]
+        //public void Test()
+        //{
+        //    Program xxx = new Program();
+        //    var resual = xxx.Grad(80);
+        //    Assert.Equal("A", resual);
 
-        }
+        //}
 
-        [Theory]
-        [InlineData(100,"A")]
-        [InlineData(70,"B")]
-        [InlineData(50,"D")]
-        [InlineData(60,"C")]
-        [InlineData(65,"C")]
-        [InlineData(75,"B")]
-        [InlineData(30,"F")]
-        [InlineData(40,"F")]
-        public void Test1(int num, string excepted)
-        {
-            Program xx = new Program();
-            var resual = xx.Grad(num);
-            Assert.Equal(excepted, resual);
-        }
+        //[Theory]
+        //[InlineData(100,"A")]
+        //[InlineData(70,"B")]
+        //[InlineData(50,"D")]
+        //[InlineData(60,"C")]
+        //[InlineData(65,"C")]
+        //[InlineData(75,"B")]
+        //[InlineData(30,"F")]
+        //[InlineData(40,"F")]
+        //public void Test1(int num, string excepted)
+        //{
+        //    Program xx = new Program();
+        //    var resual = xx.Grad(num);
+        //    Assert.Equal(excepted, resual);
+        //}
 
-        [Fact]
-        public void Testtrue()
-        {
-            Program xxx = new Program();
-            var resual = xxx.check();
-            Assert.True(resual);
+        //[Fact]
+        //public void Testtrue()
+        //{
+        //    Program xxx = new Program();
+        //    var resual = xxx.check();
+        //    Assert.True(resual);
 
-        }
+        //}
 
 
 
